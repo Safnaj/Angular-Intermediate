fe8075d2d9c27db22cbc79624670e024e8092f87
Safnaj <safnaj99@gmail.com>
2021-08-25 18:45:37 +0530

    Routing & Enhancements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { LkrFormatterPipe } from './shared/lkr-formatter.pipe';
 import { FormsModule } from '@angular/forms';
 import { ProgressBarComponent } from './shared/progress-bar/progress-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
     LkrFormatterPipe,
-    ProgressBarComponent
+    ProgressBarComponent,
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
